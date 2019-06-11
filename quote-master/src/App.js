242ffc70054f5c36978b5366b9bce3e6d4f7c288
GregJacobs82 <greg@2book.com>
2019-06-10 23:22:49 -0400

    add react-admin

--- quote-master/src/App.js
+++ quote-master/src/App.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -22,5 +23,18 @@ function App() {
     </div>
   );
 }
+
+export default App;
+*/
+
+
+
+// in src/App.js
+import React from 'react';
+import { Admin, Resource } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+const App = () => <Admin dataProvider={dataProvider} />;
 
 export default App;
