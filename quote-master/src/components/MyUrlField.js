545a8281724156c887eb0b41ef5344ff756f6020
GregJacobs82 <greg@2book.com>
2019-06-11 00:01:15 -0400

    Add PostList, UserList, and custom MyUrlField

---
+++ quote-master/src/components/MyUrlField.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import LaunchIcon from '@material-ui/icons/Launch';
+
+const styles = {
+    link: {
+        textDecoration: 'none',
+        display: 'inline-flex',
+        alignItems: 'center',
+        borderBottom: '1px solid',
+    },
+    icon: {
+        width: '0.5em',
+        paddingLeft: 2,
+    },
+};
+
+const MyUrlField = ({ record = {}, source, classes }) =>
+    <a href={record[source]} className={classes.link}>
+        {record[source]}
+        <LaunchIcon className={classes.icon} />
+    </a>;
+
+export default withStyles(styles)(MyUrlField);
