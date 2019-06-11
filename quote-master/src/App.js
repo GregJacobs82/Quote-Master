f48bca907ce7882ba4c5e3ea08946b475f74c376
GregJacobs82 <greg@2book.com>
2019-06-11 01:09:14 -0400

    Setup to connect to Real API (needs endpoint)

--- quote-master/src/App.js
+++ quote-master/src/App.js
@@ -14,10 +14,11 @@ import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 
 // DATA
-const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+// const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+import dataProvider from './dataProvider';
 
 const App = () => (
-    <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider = {dataProvider}>
+    <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
         <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
         <Resource name="users" list={UserList} icon={UserIcon}/>
     </Admin>
